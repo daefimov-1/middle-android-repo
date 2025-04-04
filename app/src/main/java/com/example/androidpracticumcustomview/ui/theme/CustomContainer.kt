ad111195382351d450c5f17cecaa4e15642e97c3
d.a.efimov <d.a.efimov@tinkoff.ru>
2025-04-04 14:30:25 -0700

    sprint_1: done

--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainer.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainer.kt
@@ -24,17 +24,30 @@ class CustomContainer @JvmOverloads constructor(
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        // TODO
-        // ...
+        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
+        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
+        setMeasuredDimension(widthSize, heightSize)
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        // TODO
-        // ...
+        val width = right - left
+        val height = bottom - top
+
+        if (childCount > 0) {
+            val firstChild = getChildAt(0)
+            firstChild.layout(0, 0, width, height / 2)
+            if (childCount > 1) {
+                val secondChild = getChildAt(1)
+                secondChild.layout(0, height / 2, width, height)
+            }
+        }
     }
 
     override fun addView(child: View) {
-        // TODO
-        // ...
+        if (childCount < 2) {
+            super.addView(child)
+        } else {
+            throw IllegalStateException("You can't add more than two views")
+        }
     }
 }
