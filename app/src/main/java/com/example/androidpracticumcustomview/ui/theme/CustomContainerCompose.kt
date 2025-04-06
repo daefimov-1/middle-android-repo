d763542e4f250d621e6ad6508c15ec1a30bb25e3
d.a.efimov <d.a.efimov@tinkoff.ru>
2025-04-06 02:52:00 -0700

    sprint_1: done

--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainerCompose.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainerCompose.kt
@@ -1,8 +1,18 @@
 package com.example.androidpracticumcustomview.ui.theme
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 
 /*
@@ -19,17 +29,50 @@ fun CustomContainerCompose(
     secondChild: @Composable (() -> Unit)?
 ) {
     // Блок создания и инициализации переменных
-    // ..
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.toFloat()
+
+    val animatedHeight1 = remember { Animatable(0f) }
+    val animatedHeight2 = remember { Animatable(0f) }
+    val alpha = remember { Animatable(0f) }
 
     // Блок активации анимации при первом запуске
     LaunchedEffect(Unit) {
-        // TODO
-        // ...
+        launch {
+            animatedHeight1.animateTo(
+                targetValue = -1 * screenHeight / 4,
+                animationSpec = tween(durationMillis = 5000)
+            )
+        }
+        launch {
+            animatedHeight2.animateTo(
+                targetValue = screenHeight / 4,
+                animationSpec = tween(durationMillis = 5000)
+            )
+        }
+        launch {
+            alpha.animateTo(
+                targetValue = 1f,
+                animationSpec = tween(durationMillis = 2000)
+            )
+        }
     }
 
-    // Основной контейнер
-    Box() {
-        // TODO
-        // ...
+    Box(
+        modifier = Modifier
+            .offset(y = animatedHeight1.value.dp)
+            .height((screenHeight / 2).dp)
+            .alpha(alpha.value)
+    ) {
+        firstChild?.invoke()
+    }
+
+    Box(
+        modifier = Modifier
+            .offset(y = animatedHeight2.value.dp)
+            .height((screenHeight / 2).dp)
+            .alpha(alpha.value)
+    ) {
+        secondChild?.invoke()
     }
 }
